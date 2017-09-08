76ffa09acfce12528bffef74a6eaaed3ab57a332
mootet <nathantr@users.noreply.github.com>
2017-09-07 22:18:01 -0700

    sss
     On branch master

--- client/main.js
+++ client/main.js
@@ -16,7 +16,7 @@ if (typeof web3 !== 'undefined') {
 } else {
     console.log('No web3? You should consider trying MetaMask!')
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  web3 = new Web3(new Web3.providers.HttpProvider("https://localhost:8545"));
 }
 
 if(!web3.isConnected()) {
